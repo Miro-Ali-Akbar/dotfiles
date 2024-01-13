96a92d8c369b18db3458dcceac5b064f75853191
Miro <miroaliakbar@gmail.com>
2024-01-13 22:11:45 +0100

    cleaned and added border buffer for cursor

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,9 +1,17 @@
+-- Makes tabs be good and sets indent_space
+local indent_space = 4
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = indent_space
+vim.opt.softtabstop = indent_space
+vim.opt.shiftwidth = indent_space
+-- Sets rownumber in gutter
 vim.cmd("set number")
 vim.cmd("set numberwidth=3")
+-- Sets clipboard
 vim.cmd("set clipboard+=unnamedplus")
+-- Makes text wrap be based on words
 vim.cmd("set wrap linebreak")
+-- Adds space for gutter
 vim.cmd("set signcolumn=yes")
+-- Sets buffer for curser between border as scroll_of_buffer
+vim.cmd("set scrolloff=5")
--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
     "lewis6991/gitsigns.nvim",
-    event = false,
+    event = "VeryLazy",
     config = function()
         require("gitsigns").setup()
     end
