85b40212ffdb509c56f0311b1d6c647cc69187df
Miro <miroaliakbar@gmail.com>
2024-01-13 16:20:08 +0100

    fixed and simplified some things along with formatting

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -28,12 +28,12 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "LSP hover" })
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "LSP defenition" })
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "LSP references" })
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "LSP rename" })
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { desc = "LSP code action" })
-            vim.keymap.set('n', '<leader>f', function()
+            vim.keymap.set('n', '<leader>lk', vim.lsp.buf.hover, { desc = "LSP hover" })
+            vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, { desc = "LSP defenition" })
+            vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, { desc = "LSP references" })
+            vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, { desc = "LSP rename" })
+            vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, { desc = "LSP code action" })
+            vim.keymap.set('n', '<leader>lf', function()
                 vim.lsp.buf.format { async = true }
             end, { desc = "LSP format" })
         end
