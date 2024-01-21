5cab4360dfddc5139c96d7708065a82354825cad
Miro Ali Akbar <miroaliakbar@gmail.com>
2024-01-21 22:50:34 +0100

    changes statusline with one that has bufferline too - Might change later for a mixed only statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,12 +1,18 @@
 return {
     lazy  = false,
 --    "nvim-lualine/lualine.nvim",
-    "echasnovski/mini.statusline",
-    config = function()
-        require("mini.statusline").setup({
-            set_vim_settings = true,
-            vim.cmd("set cmdheight=0")
-        })
-    end,
-    opts = {},
+    -- "echasnovski/mini.statusline",
+    -- config = function()
+    --     require("mini.statusline").setup({
+    --         set_vim_settings = true,
+    --     })
+    -- end,
+    -- opts = {},
+    "ojroques/nvim-hardline",
+    config = function ()
+        require('hardline').setup {
+            bufferline = true,
+        }
+        vim.cmd("set cmdheight=0")
+    end
 }
