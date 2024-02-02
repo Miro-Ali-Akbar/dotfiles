184807d710df7343a1554fa5cfa479e89d8ea53a
Miro <miroaliakbar@gmail.com>
2024-02-02 13:40:20 +0100

    added prefix names for whichkey

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -6,6 +6,16 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
         require("which-key").register({}, {})
+
+        local wk = require("which-key")
+        wk.register({
+                l = { name = "LSP" },
+                c = { name = "Toggle" },
+                f = { name = "Files" },
+                s = { name = "Search" },
+                t = { name = "Terminal" },
+            },
+            { prefix = "<leader>" })
     end,
     opts = {
 
