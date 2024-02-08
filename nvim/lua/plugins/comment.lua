42b3aff25df906c33d45f4aaf201a8772f7c6758
Miro <miroaliakbar@gmail.com>
2024-02-08 16:02:34 +0100

    added normal visual to comment <leader>cc

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,10 +1,10 @@
 return {
-    event = "VeryLazy",
+    event = "BufEnter",
     "numToStr/Comment.nvim",
     config = function()
         require("Comment").setup({
             mappings = { basic = true, extra = true },
         })
-        vim.keymap.set("x", "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle comment"})
+        vim.keymap.set({ "x", "v" }, "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle comment"})
     end,
 }
