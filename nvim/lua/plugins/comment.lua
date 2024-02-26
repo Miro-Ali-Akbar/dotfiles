bd01453332081b03ad50ed98409c14dc52946c0b
Miro Ali Akbar <miroaliakbar@gmail.com>
2024-02-26 22:07:01 +0100

    quick fixes

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -4,8 +4,9 @@ return {
     -- cmd = { TODO: add }
     config = function()
         require("Comment").setup({
-            mappings = { basic = true, extra = true },
+            mappings = { basic = false, extra = false },
         })
         vim.keymap.set({ "x", "v" }, "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle comment"})
+        vim.keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment" })
     end,
 }
--- nvim/lua/plugins/git-blame.lua
+++ nvim/lua/plugins/git-blame.lua
@@ -2,6 +2,6 @@ return {
     "f-person/git-blame.nvim",
 	event = "CursorMoved",
     config = function()
-        vim.keymap.set("n", "<leader>cb", "<cmd> GitBlameToggle <cr>", { desc = "Toggle git blame" })
+        vim.keymap.set("n", "<leader>cb", "<cmd> GitBlameToggle <cr>", { desc = "Git blame" })
     end,
 }
