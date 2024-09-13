d2d48a14e011fd32b33a30c774029166e14ed939
Miro Ali Akbar <miroaliakbar@gmail.com>
2024-09-13 19:12:45 +0200

    changed indententaion to stay in indent mode

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -33,3 +33,7 @@ vim.keymap.set("n", "<leader>sr", [[:%s/]], { desc = "Replace all" })
 -- Vim redo
 vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 vim.keymap.set("n", "<S-r>", "<nop>")
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
