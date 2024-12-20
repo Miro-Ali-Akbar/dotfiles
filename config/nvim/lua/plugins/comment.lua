58b215ca0502e23f49241080f3fa8ef2374e0497
Miro <MiroAliAkbar@gmail.com>
2024-12-20 20:05:37 +0100

    fixed lazyloading on keybind finally

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,12 +1,14 @@
 return {
     -- event = "BufEnter",
     "numToStr/Comment.nvim",
-    keys = { "<leader>cc" }, -- Might not be good as whichkey does not enable as config function has not run yet
+    keys = {
+        { "<leader>cc", nil, mode = { "x", "v", "n" }, desc = "Comment" }
+    },
     config = function()
         require("Comment").setup({
             mappings = { basic = false, extra = false },
         })
-        vim.keymap.set({ "x", "v" }, "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Toggle comment"})
+        vim.keymap.set({ "x", "v" }, "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Comment" })
         vim.keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment" })
     end,
 }
