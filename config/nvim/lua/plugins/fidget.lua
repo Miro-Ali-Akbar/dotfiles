2f77c78cdf834cbb7bfc790aa49632d6fad4373f
Miro <MiroAliAkbar@gmail.com>
2024-12-20 20:30:31 +0100

    changed figet config

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -2,10 +2,9 @@ return {
     -- Notifications in down left
     "j-hui/fidget.nvim",
     event = "BufEnter",
-    config = function()
-        -- Turn on LSP, formatting, and linting status and progress information
-        local fidget = require("fidget")
-        fidget.setup({})
-        vim.notify = fidget.notify
-    end,
+    opts = {
+        notification = {
+            override_vim_notify = true,
+        },
+    },
 }
