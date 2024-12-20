f65f99f403aead3a73884ea01365008ba00c4132
Miro <MiroAliAkbar@gmail.com>
2024-12-20 21:22:05 +0100

    added stage keybind

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -3,5 +3,7 @@ return {
     event = "BufEnter",
     config = function()
         require("gitsigns").setup()
+        vim.keymap.set("n", "<leader>fg", "<cmd>Gitsigns stage_hunk<cr>", { desc = "Stage hunk" })
+        vim.keymap.set("n", "<leader>fG", "<cmd>Gitsigns stage_buffer<cr>", { desc = "Stage buffer" })
     end
 }
