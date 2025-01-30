99221178d29196342dc5e0a4ba390d7359fee16e
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-01-30 15:53:38 +0100

    revesed git mode in terminal as i want to see alternatives

--- shell/inputrc.sh
+++ shell/inputrc.sh
@@ -1,16 +1,16 @@
 # Vi mode
-set editing-mode vi
+# set editing-mode vi
 # Show mode in vi
-set show-mode-in-prompt on
+# set show-mode-in-prompt on
 
 # Cursors
-$if term=linux
-	set vi-ins-mode-string \1\e[?0c\2
-	set vi-cmd-mode-string \1\e[?8c\2
-$else
-	set vi-ins-mode-string \1\e[6 q\2
-	set vi-cmd-mode-string \1\e[2 q\2
-$endif
+# $if term=linux
+# 	set vi-ins-mode-string \1\e[?0c\2
+# 	set vi-cmd-mode-string \1\e[?8c\2
+# $else
+# 	set vi-ins-mode-string \1\e[6 q\2
+# 	set vi-cmd-mode-string \1\e[2 q\2
+# $endif
 
 # Two tabs become one
 set show-all-if-ambiguous on
