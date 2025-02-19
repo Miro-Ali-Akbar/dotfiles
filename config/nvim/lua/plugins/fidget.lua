34e8b8b7975876772be701df9cd42eae3986daa4
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-02-19 13:55:30 +0100

    Better theming

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -1,11 +1,13 @@
 return {
     -- Notifications in down left
-    -- TODO: make text be fully visible
     "j-hui/fidget.nvim",
     event = "BufEnter",
     opts = {
         notification = {
             override_vim_notify = true,
+            window = {
+                winblend = 0,
+            }
         },
     },
 }
