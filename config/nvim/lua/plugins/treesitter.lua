e030c0641625bdffe04a6ba5cbac23f795b4b550
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-04-09 11:32:53 +0200

    added jsdoc tresitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "bash", "markdown", "markdown_inline", "typescript", "javascript", "json", "python", "latex" },
+            ensure_installed = { "c", "lua", "bash", "markdown", "markdown_inline", "typescript", "javascript", "jsdoc", "json", "python", "latex" },
             auto_installed = true,
             sync_install = false,
             highlight = { enable = true },
