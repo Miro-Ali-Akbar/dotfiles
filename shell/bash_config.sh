4225aaa33f7c72845dcd14ec41caa543d7dfe669
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-04-11 10:42:10 +0200

    changed manpager

--- shell/bash_config.sh
+++ shell/bash_config.sh
@@ -7,11 +7,12 @@ shopt -s autocd
 ErrorCodeColor() {
     [[ $? != 0 ]] && printf '\033[01;31m'
 }
-PS1='\[\033[01;32m\]$(ErrorCodeColor)\u\e[0m:\[\033[01;31m\]$(ErrorCodeColor)\w$(__git_ps1 " \[\033[01;32m\]$(ErrorCodeColor)(%s) ")\e[0m:> '
+# PS1='\[\033[01;32m\]$(ErrorCodeColor)\u\e[0m:\[\033[01;31m\]$(ErrorCodeColor)\w$(__git_ps1 " \[\033[01;32m\]$(ErrorCodeColor)(%s) ")\e[0m:> '
 
 # Alias
 alias ..="cd .."
 alias cl="clear"
 
 # manpage highlighter
-export MANPAGER="less -R --use-color -Dd+r -Du+b"
+# export MANPAGER="less -R --use-color -Dd+r -Du+b"
+export MANPAGER='nvim +Man!'
