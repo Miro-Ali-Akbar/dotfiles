fea8fb249787f332164fbf63fbf272914e83bccf
Miro Ali Akbar <Miro-Ali-Akbar@github.com>
2025-04-22 15:39:22 +0200

    changed font for statusline to be transparent at the bottom

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -2,11 +2,43 @@ return {
     lazy = false,
     "ojroques/nvim-hardline",
     config = function()
+        local colors = {
+            transparent = { gui = 'NONE', cterm = '16', cterm16 = '0' },
+            grey_medium = { gui = '#1E1E2E', cterm = '17', cterm16 = '0' },
+            grey_inactive = { gui = '#7E84A0', cterm = '61', cterm16 = '8' },
+            grey_light = { gui = '#969DBC', cterm = '69', cterm16 = '7' },
+            black = { gui = '#07070A', cterm = '16', cterm16 = '0' },
+            white = { gui = '#AEB7D9', cterm = '189', cterm16 = '15' },
+            red = { gui = '#F38BA8', cterm = '218', cterm16 = '9' },
+            yellow = { gui = '#F9E2AF', cterm = '223', cterm16 = '11' },
+        }
+        local inactive = {
+            guifg = colors.grey_inactive.gui,
+            guibg = colors.transparent.gui,
+            ctermfg = colors.grey_inactive.cterm,
+            ctermbg = colors.transparent.cterm,
+        }
+
         require("hardline").setup {
+            theme = "custom",
+            custom_theme = {
+                text =  { gui = colors.grey_medium.gui, cterm = "NONE", cterm16 = "NONE" },
+
+                normal = { gui = "#a6e3a1", cterm = "NONE", cterm16 = "NONE" },
+                insert = { gui = "#fab387", cterm = "NONE", cterm16 = "NONE" },
+                replace = { gui = "NONE", cterm = "NONE", cterm16 = "NONE" },
+                inactive_comment = inactive,
+                inactive_cursor = { gui = "NONE", cterm = "NONE", cterm16 = "NONE" },
+                inactive_menu = inactive,
+                visual = { gui = colors.red.gui, cterm = "NONE", cterm16 = "NONE" },
+                command = { gui = "NONE", cterm = "NONE", cterm16 = "NONE" },
+                alt_text = { gui = "NONE", cterm = "NONE", cterm16 = "NONE" },
+                warning = colors.yellow,
+            },
             bufferline = true,
-            sections = { -- define sections
+            sections = {     -- define sections
                 { class = 'mode', item = require('hardline.parts.git').get_item,     hide = 100 },
-                { class = 'low', item = require('hardline.parts.cwd').get_item,     hide = 100 },
+                { class = 'low',  item = require('hardline.parts.cwd').get_item,     hide = 100 },
                 { class = 'med',  item = require('hardline.parts.filename').get_item },
                 '%<',
                 { class = 'med',     item = '%=' },
@@ -18,6 +50,6 @@ return {
                 { class = 'mode',    item = require('hardline.parts.line').get_item },
             },
         }
-        -- vim.cmd("set cmdheight=0")
+        vim.cmd("set cmdheight=0")
     end
 }
