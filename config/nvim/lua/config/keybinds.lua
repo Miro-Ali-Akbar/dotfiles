1dfff1ac14a9bc8b6192a4078ad91f11c77f6fe8
Miro Ali Akbar <miroaliakbar@gmail.com>
2025-05-01 21:17:05 +0200

    added cool shorcuts

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -1,10 +1,9 @@
 vim.g.mapleader = " "
 
 -- Movement in insert mode
-vim.keymap.set("i", "<C-h>", "<Left>",  { desc = "Left" })
-vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Right" })
-vim.keymap.set("i", "<C-j>", "<Down>",  { desc = "Down" })
-vim.keymap.set("i", "<C-k>", "<Up>",    { desc = "Up" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Left" }) vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Right" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Up" })
 
 -- Movement between lines for hard wrap
 vim.keymap.set("n", "j", "gj")
@@ -17,8 +16,8 @@ vim.keymap.set("n", "<c-j>", ":wincmd j <cr>", { desc = "Window down" })
 vim.keymap.set("n", "<c-k>", ":wincmd k <cr>", { desc = "Window up" })
 
 -- Movement between buffers
-vim.keymap.set("n", "<Tab>",    "<cmd> bn <cr>", { desc = "Buffer next"})
-vim.keymap.set("n", "<S-Tab>",  "<cmd> bp <cr>", { desc = "Buffer previous"})
+vim.keymap.set("n", "<Tab>", "<cmd> bn <cr>", { desc = "Buffer next" })
+vim.keymap.set("n", "<S-Tab>", "<cmd> bp <cr>", { desc = "Buffer previous" })
 -- Buffer close -> see ../plugins/snacks.lua
 
 -- Line wrap
@@ -44,3 +43,29 @@ vim.keymap.set("n", "q:", "<cmd> q <cr>")
 
 -- Quick commands
 vim.keymap.set("n", "<leader><leader>", [[:! ]], { desc = "Run command" })
+
+-- Smart deletion from https://www.reddit.com/r/neovim/comments/1janrmf/smart_delete/
+local function smart_delete(key)
+    local l = vim.api.nvim_win_get_cursor(0)[1]                   -- Get the current cursor line number
+    local line = vim.api.nvim_buf_get_lines(0, l - 1, l, true)[1] -- Get the content of the current line
+    return (line:match("^%s*$") and '"_' or "") ..
+        key                                                       -- If the line is empty or contains only whitespace, use the black hole register
+end
+
+local keys = { "d", "dd", "x", "c", "s", "C", "S", "X" } -- Define a list of keys to apply the smart delete functionality
+
+-- Set keymaps for both normal and visual modes
+for _, key in pairs(keys) do
+    vim.keymap.set({ "n", "v" }, key, function()
+        return smart_delete(key)
+    end, { noremap = true, expr = true, desc = "Smart delete" })
+end
+
+-- Put current line above in comments
+local function comment_above()
+    vim.cmd("normal! m'")
+    vim.cmd("normal! yyP")
+    vim.cmd("normal! gcc")
+    vim.cmd("normal! `'")
+end
+vim.keymap.set("n", "<leader>ck", "m'yyPgcc`'", { remap = true, desc = "Current line commented it" })
