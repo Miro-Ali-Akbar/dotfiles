5c793ecfcd0da3372ddc0347d1f3e4ea3ffb8f09
Miro Ali Akbar <miroaliakbar@gmail.com>
2025-05-01 20:40:04 +0200

    bundeld two plugins in one file

--- config/nvim/lua/plugins/hardtime.lua
+++ config/nvim/lua/plugins/hardtime.lua
@@ -1,12 +1,24 @@
 return {
-    "m4xshen/hardtime.nvim",
-    event = "BufEnter",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    config = function()
-        require("hardtime").setup({
-            disable_mouse = false,
-        })
-        -- TODO: Does not work for some reason
-        vim.keymap.set("n", "<leader>ch", "<cmd> Hardtime toggle <cr>", { desc = "Hardtime" })
-    end,
+    {
+        enabled = true,
+        "tris203/precognition.nvim",
+        event = "BufEnter",
+        config = function()
+            require("precognition").setup({})
+            vim.keymap.set("n", "<leader>cp", "<cmd> Precognition toggle <cr>", { desc = "Precognition" })
+        end,
+
+    },
+    {
+        "m4xshen/hardtime.nvim",
+        event = "BufEnter",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        config = function()
+            require("hardtime").setup({
+                disable_mouse = false,
+            })
+            -- TODO: Does not work for some reason
+            vim.keymap.set("n", "<leader>ch", "<cmd> Hardtime toggle <cr>", { desc = "Hardtime" })
+        end,
+    }
 }
--- config/nvim/lua/plugins/precognition.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "tris203/precognition.nvim",
-    event = "BufEnter",
-    config = function()
-        require("precognition").setup({})
-        vim.keymap.set("n", "<leader>cp", "<cmd> Precognition toggle <cr>", { desc = "Precognition" })
-    end,
-}
