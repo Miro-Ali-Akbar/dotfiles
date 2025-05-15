c91765361de2e42855e3abe793e12bfaf7e38543
Miro Ali Akbar <miroaliakbar@gmail.com>
2025-05-15 14:17:55 +0200

    moved from vim cmd to vim.opt

--- config/nvim/lua/config/settings.lua
+++ config/nvim/lua/config/settings.lua
@@ -4,35 +4,49 @@ vim.opt.expandtab = true
 vim.opt.tabstop = indent_space
 vim.opt.softtabstop = indent_space
 vim.opt.shiftwidth = indent_space
+
 -- Sets row number in gutter
-vim.cmd("set number")
-vim.cmd("set numberwidth=3")
+vim.opt.number = true
+vim.opt.numberwidth = 3
+
 -- Sets clipboard
-vim.cmd("set clipboard+=unnamedplus")
+vim.opt.clipboard = "unnamedplus"
+
 -- Makes text wrap be based on words
-vim.cmd("set wrap linebreak")
-vim.cmd("set breakindent")
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
 -- Adds space for gutter
-vim.cmd("set signcolumn=yes")
+vim.opt.signcolumn = "yes"
+
 -- Sets buffer for cursor between border as scroll_of_buffer
-vim.cmd("set scrolloff=5")
+vim.opt.scrolloff = 5
+
 -- Vim search to be case sensitive when relevant
-vim.cmd("set ignorecase")
-vim.cmd("set smartcase")
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 -- Vim replace - Becomes responsive
-vim.cmd("set inccommand=nosplit")
+vim.opt.inccommand = "nosplit"
+
 -- Makes nvim faster somehow?
 vim.loader.enable()
+
 -- Persistent undo
-vim.cmd("set undofile")
--- Mouse = good
-vim.cmd("set mouse=a")
+vim.opt.undofile = true
+
+-- Mouse = good TODO: More
+vim.opt.mouse = "a"
+
 -- spell 
 vim.opt.spelllang = "en_us,sv"
 vim.opt.spell = true
 vim.opt.spelloptions = "camel"
+
 -- Conceal for better markdown and tex
-vim.cmd("set conceallevel=2")
+vim.opt.conceallevel = 2
+
 -- Folds
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
