b7e8f58a7ba7c410ab736eb5906db91ca2be7921
Miro Ali Akbar <miroaliakbar@gmail.com>
2025-05-22 15:04:54 +0200

    changed toggle to be non deprecated

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -4,12 +4,11 @@ return {
     -- cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },-- TODO: fix so cmd actually works
     dependencies = {
         "williamboman/mason.nvim",
-    	"williamboman/mason-lspconfig.nvim",
+        "williamboman/mason-lspconfig.nvim",
 
         "hrsh7th/cmp-nvim-lsp",
         "folke/lazydev.nvim",
 
-        "WhoIsSethDaniel/toggle-lsp-diagnostics.nvim",
 
         -- 'akinsho/flutter-tools.nvim', -- flutter
     },
@@ -19,31 +18,29 @@ return {
         require("mason").setup()
 
         local mason_lspconfig = require("mason-lspconfig")
-        local lspconfig = require("lspconfig")
 
-        mason_lspconfig.setup_handlers({
-            function(server)
-                lspconfig[server].setup({})
-            end,
-        })
         mason_lspconfig.setup({
             ensure_installed = { "lua_ls", "clangd", "ts_ls", "bashls", "pyright" },
-    		automatic_installation = true,
-    	})
+            automatic_installation = true,
+        })
 
-        function lsp_format()
+        vim.diagnostic.config({ virtual_text = true, })
+
+        function Lsp_toggle_diagnostic()
+            vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+        end
+
+        function Lsp_format()
             vim.lsp.buf.format { async = true }
         end
 
-        vim.keymap.set("n",             "<leader>lk",   vim.lsp.buf.hover,              { desc = "Hover" })
-        vim.keymap.set("n",             "<leader>ld",   vim.lsp.buf.definition,         { desc = "Defenition" })
-        vim.keymap.set("n",             "<leader>lr",   vim.lsp.buf.references,         { desc = "References" })
-        vim.keymap.set("n",             "<leader>ln",   vim.lsp.buf.rename,             { desc = "Rename" })
-        vim.keymap.set({ "n", "v" },    "<leader>la",   vim.lsp.buf.code_action,        { desc = "Code action" })
-        vim.keymap.set("n",             "<leader>lD",   vim.diagnostic.open_float,      { desc = "Diagnostic" })
-        vim.keymap.set("n",             "<leader>lf",   "<cmd>lua lsp_format()<cr>",  { desc = "Format", noremap = true, })
-
-        require("toggle_lsp_diagnostics").init()
-        vim.keymap.set("n",             "<leader>cl",   "<cmd> ToggleDiag <cr>",        { desc = "Lsp", silent = true })
+        vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, { desc = "Hover" })
+        vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "Defenition" })
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "References" })
+        vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "Rename" })
+        vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code action" })
+        vim.keymap.set("n", "<leader>lD", vim.diagnostic.open_float, { desc = "Diagnostic" })
+        vim.keymap.set("n", "<leader>lf", "<cmd>lua Lsp_format()<cr>", { desc = "Format", noremap = true, })
+        vim.keymap.set('n', '<leader>cl', "<cmd>lua Lsp_toggle_diagnostic()<cr>", { desc = "Lsp diagnostics" })
     end
 }
