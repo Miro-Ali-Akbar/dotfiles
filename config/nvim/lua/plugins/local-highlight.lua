0798888c56c9f9e49b6beb3f5a4f869b578f2e1b
Miro Ali Akbar <miroaliakbar@gmail.com>
2025-05-26 16:13:43 +0200

    toggleble

--- config/nvim/lua/plugins/local-highlight.lua
+++ config/nvim/lua/plugins/local-highlight.lua
@@ -1,6 +1,8 @@
 return {
     "tzachar/local-highlight.nvim",
     config = function()
-        require('local-highlight').setup()
+        require('local-highlight').setup({
+            vim.keymap.set("n", "<leader>ch", "<cmd>LocalHighlightToggle<cr>", { desc = "Highlight current word" })
+        })
     end
 }
