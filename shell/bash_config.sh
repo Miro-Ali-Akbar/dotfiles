8c57ce505accea395629d5b01feb970a0a1ffa4d
Miro Ali Akbar <113117782+Miro-Ali-Akbar@users.noreply.github.com>
2026-08-13 20:55:07 +0200

    no sleep

--- shell/bash_config.sh
+++ shell/bash_config.sh
@@ -30,6 +30,7 @@ PROMPT_COMMAND=build_prompt
 alias ..="cd .."
 alias cl="clear"
 alias REPL="NVIM_REPL=1 nvim"
+alias nosleep='systemd-inhibit --what=handle-lid-switch:sleep:idle --why="no sleep" sleep infinity'
 eval "$(fzf --bash)"
 export EDITOR=nvim
 
